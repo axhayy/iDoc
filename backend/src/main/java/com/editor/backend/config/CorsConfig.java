52cbf5e256f0e4725c99414a21584c5d39cbd753
Akshay <akshaykanha31@gmail.com>
2026-06-12 16:20:44 +0530

    Allow deployed frontend in CORS

--- backend/src/main/java/com/editor/backend/config/CorsConfig.java
+++ backend/src/main/java/com/editor/backend/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                 = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173"));
+                List.of("http://localhost:5173", "https://i-doc-rho.vercel.app/"));
 
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
